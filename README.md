04e0c3a6410345c58fd00698fab242f079a73105
arvislacis <arvis.lacis@gmail.com>
2013-06-17 09:52:29 +0300

    Mazie draugi v0.4.0:
    * Nomainīts paplašinājuma nosaukums, ikonas un license (tagad - GPLv3);
    * Pārstrukturizēts un izmainīts paplašinājuma dizains;
    * Nedaudz izmainīta paplašinājuma funkcionalitāte;
    * Atjauninātas jQuery un Bootstrap bibliotēkas.

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-# Draugiem.lv profila jaunumi #
+# Mazie draugi #
 
-**Draugiem.lv profila jaunumi** ir Google Chrome paplašinājums, kas paziņojumu veidā attēlo jaunumus (vēstules, galerijas, statistika utml.) lietotāja Draugiem.lv profilā.
+## License ##
 
-## Licenses ##
+Mazie draugi - Paplašinājums, kas informē par jaunumiem Draugiem.lv profilā.
+Copyright (C) 2013 Arvis Lācis
 
-- **[Draugiem.lv profila jaunumi - source code](https://github.com/arvislacis/draugiemlv_pj)** by Arvis Lācis is licensed under a [CC BY-SA 3.0 License](http://creativecommons.org/licenses/by-sa/3.0/). (Copyright 2013 Arvis Lācis)
-	Files: `manifest.json`, `js/skripts.js`, `html/iestatijumi.html`, `js/iestatijumi.js`.
-- **[Draugiem.lv profila jaunumi - extension icons](https://github.com/arvislacis/draugiemlv_pj)** by Edgars Kupčs is licensed under a [CC BY-SA 3.0 License](http://creativecommons.org/licenses/by-sa/3.0/). (Copyright 2013 Edgars Kupčs)
-	Files: `img/ikona16.png`, `img/ikona48.png`, `img/ikona128.png`.
-- **[Glyphicons Free](http://glyphicons.com/)** is licensed under a [CC BY 3.0 License](http://creativecommons.org/licenses/by/3.0/). (Copyright 2010-2013 Jan Kovařík)
-	Files: `img/glyphicons-halflings.png`, `img/glyphicons-halflings-white.png`.
-- **[jQuery code](http://jquery.com/)** is licensed under the [MIT License](https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt). (Copyright 2013 jQuery Foundation and other contributors)
-	Files: `js/jquery.min.js`.
-- **[Boostrap code](http://twitter.github.io/bootstrap/)** is licensed under the [Apache License v2.0](http://www.apache.org/licenses/LICENSE-2.0). (Copyright 2012 Twitter, Inc.)
-	Files: `js/bootstrap.min.js`, `css/bootstrap.min.css`.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- js/skripts.js
+++ js/skripts.js
@@ -1,5 +1,22 @@
-// (CC BY-SA) 2013 Arvis Lācis
-// arvis.lacis@gmail.com | http://twitter.com/arvislacis (@arvislacis) | http://al.id.lv
+/*
+
+Mazie draugi - Paplašinājums, kas informē par jaunumiem Draugiem.lv profilā.
+Copyright (C) 2013 Arvis Lācis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 /* jshint bitwise:true, curly:true, eqeqeq:true, forin:true, globalstrict:true, newcap:true, noarg:true, noempty:true, onevar: true, undef:true, unused:true, browser:true, jquery:true, indent:4 */
 /* global chrome:false, webkitNotifications:false */
@@ -7,68 +24,95 @@
 "use strict";
 
 $(document).ready(function () {
-	function zina (attels, nosaukums, teksts, taimeris) {
-		var pazinojums = webkitNotifications.createNotification(attels, nosaukums, teksts);
+	function zina () {
+		$.get("http://www.draugiem.lv/messages/", function (data) {
+			var info = $(data).find(".unread");
 
-		pazinojums.show();
+			$(info).each(function () {
+				var pazinojums = webkitNotifications.createNotification("",
+					"Vēstule no " + $(this).find(".ml_user").text() + " (" + $(this).find(".ml_time").text() + ")",
+					$(this).find(".ml_subject").text());
 
-		if ((taimeris === true) || (taimeris === undefined)) {
-			setTimeout(function () {
-				pazinojums.cancel();
-			}, 5000);
-		}
-	}
+				pazinojums.show();
 
-	function atjaunot () {
-		$.get("http://www.draugiem.lv", function (data) {
-			chrome.storage.local.get (null, function (iest) {
-				var vards = $(data).find("#my-name a").text(),
-					attels = $(data).find(".picture img").prop("src").replace("nm", "i"),
-					teksts = " | ",
-					dati = [
-						["Vēstules", $(data).find("#menuMessages .badge").text(), iest.dr_vestules],
-						["Profila jaunumi", $(data).find("#myProfileNews .badge").text(), iest.dr_jaunumi],
-						["Statistika", $(data).find("#menuVisitors .badge").text(), iest.dr_statistika],
-						["Galerijas", $(data).find("#menuGallery .badge").text(), iest.dr_galerijas],
-						["Grupas", $(data).find("#menuGroups .badge").text(), iest.dr_grupas],
-						["Dienasgrāmatas", $(data).find("#menuBlogs .badge").text(), iest.dr_dienasgramatas],
-						["Citi", $(data).find("#menuCiti .badge").text(), iest.dr_citi],
-						["Kalendārs", $(data).find("#calendarBox .badge").text(), iest.dr_kalendars],
-						["Draugi online", $(data).find("#menuFriends .badge").text(), iest.dr_online]
-					];
-
-				$.each(dati, function (i) {
-					if ((dati[i][1] !== "") && ((dati[i][2] === true) || (dati[i][2] === undefined))) {
-						teksts = teksts + dati[i][0] + ": " + dati[i][1] + " | ";
-					}
-				});
-
-				if (((vards === iest.dr_lietotajs) || (iest.dr_lietotajs === "") || (iest.dr_lietotajs === undefined)) && (teksts !== " | ")) {
-					if (vards !== "") {
-						if (iest.dr_foto === false) {
-							zina("", vards, teksts, iest.dr_aizvert);
-						} else {
-							zina(attels, vards, teksts, iest.dr_aizvert);
-						}
-					} else {
-						zina("", "Kļūda iegūstot datus", "Atvainojiet, mēģinot iegūt datus no Draugiem.lv profila, radās kļūda. Pārliecinieties, ka Jums ir patstāvīgs savienojums ar Draugiem.lv profilu.", false);
-					}
-				}
+				setTimeout(function () {
+					pazinojums.cancel();
+				}, 10000);
 			});
 		});
 	}
 
+	function atjaunot () {
+		chrome.browserAction.setBadgeBackgroundColor({color: "#009900"});
+		chrome.browserAction.setBadgeText({text: "..."});
+
+		$.get("http://www.draugiem.lv", function (data) {
+				if ($(data).find("#my-name a").text() === "") {
+					chrome.browserAction.setBadgeBackgroundColor({color: "#000000"});
+					chrome.browserAction.setBadgeText({text: "XXX"});
+
+					$("#info").html("<div class='btn-group btn-group-vertical'><a class='btn btn-success' href='http://www.draugiem.lv' target='_window'>Pieslēgties Draugiem.lv<strong></div>");
+				}
+
+				var attels = $(data).find(".picture img").prop("src").replace("nm", "i"),
+				info = "<div class='btn-group btn-group-vertical'><a class='btn btn-mini' href='http://www.draugiem.lv' target='_window'><strong>" + $(data).find("#my-name a").text() +"</strong></a><a class='btn' href='http://www.draugiem.lv/account/' target='_window'><img src='" + attels + "'></a>",
+
+				d = [
+/* 00 */				["Profila jaunumi", "", $(data).find("#myProfileNews .badge").text()],
+/* 01 */				["Uzaicinājumi", "invitations/", $(data).find(".friendshipIcon .badge").text()],
+/* 02 */				["Galerijas", "gallery/", $(data).find("#menuGallery .badge").text()],
+/* 03 */				["Aplikācijas", "applications/", $(data).find("#menuApp .badge").text()],
+/* 04 */				["Grupas", "groups/", $(data).find("#menuGroups .badge").text()],
+/* 05 */				["Lapas", "lapas/", $(data).find("#menuLapas .badge").text()],
+/* 06 */				["Mūzika", "music/", $(data).find("#menuMusic .badge").text()],
+/* 07 */				["Citi", "", $(data).find("#menuCiti .open .badge").text()],
+/* 08 */				["Statistika", "visitors/", $(data).find("#menuVisitors .badge").text()],
+/* 09 */				["Draugi online", "friends/", $(data).find("#menuFriends .badge").text()],
+/* 10 */				["Dienasgrāmatas", "blogs/", $(data).find("#menuBlogs .badge").text()],
+/* 11 */				["Vēstules", "messages/", $(data).find("#menuMessages .badge").text()],
+					],
+
+					statistika = d[8][2],
+					vestules = d[11][2];
+
+				$.each(d, function (i) {
+					if (d[i][2] !== "") {
+						info = info + "<a class='btn btn-mini' href='http://www.draugiem.lv/" + d[i][1] + "' target='_window'>" + d[i][0] + ": <strong>" + d[i][2] + "</strong></a>";
+					}
+				});
+
+				$("#info").html(info + "</div>");
+
+				if ((d[2][2] !== "") || (d[7][2] !== "") || (d[10][2] !== "")) {
+					chrome.browserAction.setBadgeBackgroundColor({color: "#999900"});
+				}
+
+				if ((d[0][2] !== "") || (d[1][2] !== "") || (d[4][2] !== "") || (d[5][2] !== "")) {
+					chrome.browserAction.setBadgeBackgroundColor({color: "#000099"});
+				}
+
+				if (statistika === "") {
+					statistika = "0";
+				}
+
+				if (vestules === "") {
+					vestules = "0";
+				} else {
+					chrome.browserAction.setBadgeBackgroundColor({color: "#990000"});
+					zina();
+				}
+
+				chrome.browserAction.setBadgeText({text: statistika + " " + vestules});
+			});
+	}
+
+	atjaunot();
+
 	chrome.alarms.onAlarm.addListener(function (alarm) {
-		if (alarm.name === "dr_atjaunot") {
+		if (alarm.name === "mazie_draugi") {
 			atjaunot();
 		}
 	});
 
-	chrome.storage.local.get (null, function (iest) {
-		if (iest.dr_iestatijumi === true) {
-			chrome.alarms.create("dr_atjaunot", {periodInMinutes: iest.dr_laiks});
-		} else {
-			chrome.alarms.create("dr_atjaunot", {periodInMinutes: 5});
-		}
-	});
+	chrome.alarms.create("mazie_draugi", {periodInMinutes: 2});
 });
